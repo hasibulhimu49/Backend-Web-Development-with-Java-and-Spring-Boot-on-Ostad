ce9886b71749420cda435966b3f6ced585bbee20
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2025-08-11 23:13:00 +0600

    Add files via upload

---
+++ Assignment_1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);
+
+        System.out.print("Enter full name: ");
+        String name=s.nextLine();
+
+        System.out.println(name);
+        System.out.println("It is a versatile, platform-independent language widely used in web, mobile, and enterprise applications.");
+        System.out.println("My career goal is to become a skilled and innovative software engineer");
+    }
+}

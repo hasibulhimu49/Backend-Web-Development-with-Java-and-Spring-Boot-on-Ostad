7cbd04d7887aa6d0295e2ee5b78893d5370e84aa
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2025-11-01 15:26:31 +0600

    Initial commit with project files

---
+++ BLM/src/main/java/com/blm/BlmApplication.java
@@ -0,0 +1,14 @@
+package com.blm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlmApplication.class, args);
+		System.out.println("Project Starting...");
+	}
+
+}

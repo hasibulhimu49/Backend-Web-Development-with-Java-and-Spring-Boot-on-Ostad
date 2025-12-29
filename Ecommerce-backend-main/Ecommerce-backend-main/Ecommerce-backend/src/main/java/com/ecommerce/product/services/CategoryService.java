3e9bd3f402da96f4b7c13f675f5b885814beb2df
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2025-12-29 23:30:15 +0600

    Delete Ecommerce-backend-main/Ecommerce-backend-main/Ecommerce-backend directory

--- Ecommerce-backend-main/Ecommerce-backend-main/Ecommerce-backend/src/main/java/com/ecommerce/product/services/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.product.services;
-
-import com.ecommerce.product.dto.request.CategoryCreateRequest;
-import com.ecommerce.product.entity.Category;
-
-public interface CategoryService {
-    Category create(CategoryCreateRequest request);
-}

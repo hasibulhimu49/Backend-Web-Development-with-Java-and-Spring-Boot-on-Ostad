3494f4a3fd4fd61011396383d8902eada3810ef2
Mohammad Hasibul Hasan <109550801+hasibulhimu49@users.noreply.github.com>
2025-12-30 18:06:21 +0600

    Add files via upload

---
+++ Assignment_10/src/main/java/com/example/first/FirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //2
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+}
